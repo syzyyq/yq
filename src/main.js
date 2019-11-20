d8e28023aed9d54ddcd9bc76553927e2fb85edf0
syzyyq <2120784266@qq.com>
2019-11-20 15:24:09 +0800

    zy

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import '@/assets/styles/reset.css'
-import '@/assets/styles/border.css'
-import '@/assets/styles/iconfont.css'
+// 导入fastclick解决手机或浏览器可能的300毫秒延迟的问题
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// 导入VueAwesomeSwiper 组件
+import './assets/styles/reset.css'
+// 重置所有样式
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
+// 解决移动端1像素边框问题
+import 'swiper/dist/css/swiper.css'
+// 导入swiper样式表
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+// 调用fastClick绑定到document.body上
+Vue.use(VueAwesomeSwiper)
+// 调用VueAwesomeSwiper
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
        <home-header></home-header>
-    </div>
+       <home-swiper></home-swiper>
+   </div>
 </template>
 
 <script>
 import HomeHeader from './components/header'
+import HomeSwiper from './components/Swiper'
 export default {
     name: 'Home',
     components: {
-       HomeHeader
+       HomeHeader,
+       HomeSwiper
     }
 }
 </script>

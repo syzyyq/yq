f0d6af7d58b5c5107ca5322bf0923ec12e95d3c1
syzyyq <2120784266@qq.com>
2019-12-11 16:02:17 +0800

    12.11

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="list">
+      <!-- 通过v-for循环 遍历传递进来的cities里的数据 -->
+      <li class="item" 
+      v-for="(item,key) of cities" 
+      :key="key">{{key}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet',
+  props:{
+    cities: Object
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .list
+    display flex
+    flex-direction:column
+    justify-content:center
+    position:absolute
+    top: 1.58rem
+    right:0
+    bottom:0
+    width:.4rem
+    .item
+      text-align:center
+      line-height: .4rem
+      color $bgColor
+</style>
